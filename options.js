568e4af68fc2f02751f2740921cebb1934520377
sorogovets.nikolay <sorogovets.nikolay@gmail.com>
2019-06-09 00:35:14 +0300

    Added Icons, added view in web links after upload

--- options.js
+++ options.js
@@ -1,45 +1,45 @@
 const DEFAULT_SETTINGS = {
-    domain: '',
-    username: ''
+	folder_url: '',
+	username: ''
 };
 // Saves options to chrome.storage
 function save_options(event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    const domain = $('#domain-url').val();
-    const username = $('#username').val();
+	const folder_url = $('#folder-url').val();
+	const username = $('#username').val();
 
-    chrome.storage.sync.set(
-        {
-            domain,
-            username
-        },
-        function() {
-            // Update status to let user know options were saved.
-            $('#status').text('Settings saved.');
+	chrome.storage.sync.set(
+		{
+			folder_url,
+			username
+		},
+		function () {
+			// Update status to let user know options were saved.
+			$('#status').text('Settings saved.');
 
-            setTimeout(function() {
-                $('#status').text('');
-            }, 2000);
-        }
-    );
+			setTimeout(function () {
+				$('#status').text('');
+			}, 2000);
+		}
+	);
 }
 
 // Restores extension settings
 // stored in chrome.storage.
 function restore_options() {
-    // Use default values localhost settings
-    chrome.storage.sync.get(
-        {
-            domain: DEFAULT_SETTINGS.domain,
-            username: DEFAULT_SETTINGS.username
-        },
-        function(items) {
-            $('#domain-url').val(items.domain);
-            $('#username').val(items.username);
-        }
-    );
+	// Use default values localhost settings
+	chrome.storage.sync.get(
+		{
+			folder_url: DEFAULT_SETTINGS.folder_url,
+			username: DEFAULT_SETTINGS.username
+		},
+		function (items) {
+			$('#folder-url').val(items.folder_url);
+			$('#username').val(items.username);
+		}
+	);
 
-    $('#settingsForm').submit(save_options);
+	$('#settingsForm').submit(save_options);
 }
 $(document).ready(restore_options);
--- popup.js
+++ popup.js
@@ -2,215 +2,291 @@ let settings = {};
 let data = {};
 
 const DEFAULT_SETTINGS = {
-    client_id: '384905528545-qdgr4vg5g577uh5rrv8apj2b3v54bj68.apps.googleusercontent.com',
-    domain: '',
-    username: ''
+	client_id: '384905528545-qdgr4vg5g577uh5rrv8apj2b3v54bj68.apps.googleusercontent.com',
+	folder_url: '',
+	username: ''
 };
 
+const CONSTANTS = {
+	DEFAULT_SETTINGS,
+	IDS: {
+		AUTHORIZE_BTN: '#authorize_button',
+		GET_FOLDERS_BUTTON: '#get_subfolders_button',
+		CURRENT_FOLDER: '#currentFolder',
+		TAKE_SCREENSHOT_BTN: '#take-screenshot-button',
+		SCREENSHOT_CONTAINER: '#screenshot',
+		SCREENSHOT_PREVIEW: '#screenshot-preview',
+		FOLDER_LIST: '#folders_list',
+		UPLOAD_BUTTON: '#upload_screenshot_button'
+	},
+	APIS: {
+		GET_FILES: 'https://www.googleapis.com/drive/v3/files',		
+		MULTIPART_UPLOAD: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart'
+	}
+
+}
+
 function takeScreenshot() {
-    chrome.tabs.captureVisibleTab(null, { format: 'png' }, dataUrl => {
-        $('#screenshot-preview').attr('src', dataUrl);
+	$(CONSTANTS.IDS.TAKE_SCREENSHOT_BTN).disable();
+	chrome.tabs.captureVisibleTab(null, { format: 'png' }, dataUrl => {
+		$(CONSTANTS.IDS.SCREENSHOT_PREVIEW).attr('src', dataUrl);
+		$(CONSTANTS.IDS.SCREENSHOT_CONTAINER).show();
 
-        $('#screenshot').removeClass('hidden');
-        var today = new Date();
-        var now = today.toISOString().substring(0, 10);
+		const today = new Date();
+		const now = today.toISOString().substring(0, 10);
+		const filename = `${settings.username}_${now}.png`;
 
-        var filename = `${settings.username}_${now}.png`;
-        $('#screenshot-preview').attr('alt', filename);
+		$(CONSTANTS.IDS.SCREENSHOT_PREVIEW).attr('alt', filename);
 
-        console.log(filename);
+		console.log(filename);
 
-        data = {
-            ...data,
-            href: dataUrl,
-            filename: filename
-        };
-    });
+		data = {
+			...data,
+			href: dataUrl,
+			filename: filename
+		};
+
+		$(CONSTANTS.IDS.TAKE_SCREENSHOT_BTN).enable();
+	});
 }
 
 function getIdFromUrl(url) {
-    return url.match(/[-\w]{25,}/)[0];
+	return url.match(/[-\w]{25,}/)[0];
 }
 
 function getListOfSubFolders(folderId = null) {
-    const { domain } = settings;
+	$(CONSTANTS.IDS.GET_FOLDERS_BUTTON).disable();
 
-    if (folderId === null) {
-        $('#currentFolder').html('<strong>Root</strong>');
-        data = {
-            ...data,
-            selectedFolder: { id: getIdFromUrl(domain) }
-        };
-    }
+	const { folder_url } = settings;
 
-    folderId = folderId || getIdFromUrl(domain);
+	if (folderId === null) {
+		$(CONSTANTS.IDS.CURRENT_FOLDER).html('<strong>Root</strong>');
+		data = {
+			...data,
+			selectedFolder: { id: getIdFromUrl(folder_url) }
+		};
+	}
 
-    $.ajax({
-        url: 'https://www.googleapis.com/drive/v3/files',
-        method: 'GET',
-        crossDomain: true,
-        headers: {
-            Authorization: 'Bearer ' + data.token
-        },
-        data: {
-            corpora: 'user',
-            q: `mimeType = 'application/vnd.google-apps.folder' and '${folderId}' in parents`,
-            supportsTeamDrives: true
-        }
-    })
-        .done(response => {
-            data = {
-                ...data,
-                folders: response.files
-            };
+	folderId = folderId || getIdFromUrl(folder_url);
 
-            renderFolderList(response.files);
-        })
-        .fail(function(jqXHR, textStatus) {
-            alert('Request failed: ' + textStatus);
-        });
+	$.ajax({
+		url: CONSTANTS.APIS.GET_FILES,
+		method: 'GET',
+		crossDomain: true,
+		headers: {
+			Authorization: 'Bearer ' + data.token
+		},
+		data: {
+			corpora: 'user',
+			q: `mimeType = 'application/vnd.google-apps.folder' and '${folderId}' in parents`,
+			supportsTeamDrives: true
+		}
+	})
+		.done(response => {
+			data = {
+				...data,
+				folders: response.files
+			};
+
+			renderFolderList(response.files);
+		})
+		.fail(function (jqXHR, textStatus) {
+			alert('Request failed: ' + textStatus);
+		})
+		.always(() => {
+			$(CONSTANTS.IDS.GET_FOLDERS_BUTTON).enable();
+		});
 }
 
 function renderFolderList(folders) {
-    $('#folders_list').empty();
+	$(CONSTANTS.IDS.FOLDER_LIST).empty();
 
-    folders.map(folder => {
-        let element = $(`<li data-id="${folder.id}" class="folder">${folder.name}</li>`);
-        element.click(folder.id, event => {
-            data = {
-                ...data,
-                selectedFolder: folder
-            };
-            getListOfSubFolders(event.data);
-            updateSelectedFolder();
-        });
-        $('#folders_list').append(element);
-    });
+	folders.map(folder => {
+		let element = $(`<li data-id="${folder.id}" class="folder">${folder.name}</li>`);
+		element.click(folder.id, event => {
+			data = {
+				...data,
+				selectedFolder: folder
+			};
+			getListOfSubFolders(event.data);
+			updateSelectedFolder();
+		});
+		$(CONSTANTS.IDS.FOLDER_LIST).append(element);
+	});
 }
 
 function updateSelectedFolder() {
-    const { selectedFolder } = data;
-    if (selectedFolder) {
-        $('#currentFolder').append(` > <strong>${selectedFolder.name}</strong>`);
-    }
+	const { selectedFolder } = data;
+	if (selectedFolder) {
+		$(CONSTANTS.IDS.CURRENT_FOLDER).append(` > <strong>${selectedFolder.name}</strong>`);
+	}
 }
 
 function uploadScreenshot() {
-    const { selectedFolder, href, filename } = data;
+	$(CONSTANTS.IDS.UPLOAD_BUTTON).disable();
+	const { selectedFolder, href, filename } = data;
 
-    const base64Data = href.replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
+	const base64Data = href.replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
 
-    const metadata = {
-        name: filename,
-        mimeType: 'image/png',
-        parents: [selectedFolder.id]
-    };
+	const metadata = {
+		name: filename,
+		mimeType: 'image/png',
+		parents: [selectedFolder.id]
+	};
 
-    const boundary = '-------314159265358979323846';
-    const delimiter = '\r\n--' + boundary + '\r\n';
-    const close_delimiter = '\r\n--' + boundary + '--';
-    var contentType = metadata.mimeType || 'application/octet-stream';
-    var multipartRequestBody =
-        delimiter +
-        'Content-Type: application/json\r\n\r\n' +
-        JSON.stringify(metadata) +
-        delimiter +
-        'Content-Type: ' +
-        contentType +
-        '\r\n' +
-        'Content-Transfer-Encoding: base64\r\n' +
-        '\r\n' +
-        base64Data +
-        close_delimiter;
+	const boundary = '-------314159265358979323846';
+	const delimiter = '\r\n--' + boundary + '\r\n';
+	const close_delimiter = '\r\n--' + boundary + '--';
+	const contentType = metadata.mimeType || 'application/octet-stream';
+	const multipartRequestBody =
+		delimiter +
+		'Content-Type: application/json\r\n\r\n' +
+		JSON.stringify(metadata) +
+		delimiter +
+		'Content-Type: ' +
+		contentType +
+		'\r\n' +
+		'Content-Transfer-Encoding: base64\r\n' +
+		'\r\n' +
+		base64Data +
+		close_delimiter;
 
-    $.ajax({
-        url: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart',
-        method: 'POST',
-        crossDomain: true,
-        headers: {
-            Authorization: 'Bearer ' + data.token,
-            'Content-Type': `multipart/related; boundary=${boundary}`
-        },
-        data: multipartRequestBody
-    })
-        .done(response => {
-            console.log(response);
-            $('#msg-success').show();
-        })
-        .fail(function(jqXHR, textStatus) {
-            alert('Request failed: ' + textStatus);
-            $('#msg-error').show();
-        });
+	$.ajax({
+		url: CONSTANTS.APIS.MULTIPART_UPLOAD,
+		method: 'POST',
+		crossDomain: true,
+		headers: {
+			Authorization: 'Bearer ' + data.token,
+			'Content-Type': `multipart/related; boundary=${boundary}`
+		},
+		data: multipartRequestBody
+	})
+		.done(response => {
+			console.log(response);
+
+			data = {
+				...data,
+				uploadedFile: response
+			};
+
+			$('#msg-success').show();
+			$('#view_link').html(response.name);
+			getFileUrls();
+		})
+		.fail(function (jqXHR, textStatus) {
+			alert('Request failed: ' + textStatus);
+			$('#msg-error').show();
+		})
+		.always(() => {
+		
+		});
+}
+
+function getFileUrls() {
+	const { uploadedFile } = data;
+	$.ajax({
+		url: `${CONSTANTS.APIS.GET_FILES}/${uploadedFile.id}?fields=webViewLink`,
+		method: 'GET',
+		crossDomain: true,
+		headers: {
+			Authorization: 'Bearer ' + data.token
+		}
+	})
+	.done(response => {
+
+		data = {
+			...data,
+			uploadedFile: {
+				...uploadedFile,
+				viewLink: response.webViewLink,
+			}
+		};
+
+		$('#view_link').attr('href', response.webViewLink);
+		$('#view_link').click(() => {
+			chrome.tabs.create({
+				url:  response.webViewLink
+			});
+		})
+	})
+	.fail(function (jqXHR, textStatus) {
+		alert('Request failed: ' + textStatus);
+		$('#msg-error').show();
+	})
+	.always(() => {
+		$(CONSTANTS.IDS.UPLOAD_BUTTON).enable();
+	})
+
 }
 
 /**
  * Setting initialization
  */
 function loadSettings() {
-    chrome.storage.sync.get(
-        {
-            domain: DEFAULT_SETTINGS.domain,
-            username: DEFAULT_SETTINGS.username
-        },
-        function(items) {
-            settings = items;
-            data = {
-                ...data,
-                selectedFolder: { id: getIdFromUrl(items.domain) }
-            };
-            handleClientLoad();
-        }
-    );
+	chrome.storage.sync.get(
+		{
+			folder_url: DEFAULT_SETTINGS.folder_url,
+			username: DEFAULT_SETTINGS.username
+		},
+		function (items) {
+			settings = items;
+			data = {
+				...data,
+				selectedFolder: { id: getIdFromUrl(items.folder_url) }
+			};
+			handleClientLoad();
+		}
+	);
 }
 
 function getAuthToken(options) {
-    chrome.identity.getAuthToken({ interactive: options.interactive }, options.callback);
+	chrome.identity.getAuthToken({ interactive: options.interactive }, options.callback);
 }
 /**
  *  On load, called to load the auth2 library and API client library.
  */
 function handleClientLoad() {
-    getAuthToken({
-        interactive: false,
-        callback: getAuthTokenSilentCallback
-    });
+	getAuthToken({
+		interactive: false,
+		callback: getAuthTokenSilentCallback
+	});
 }
 
 function getAuthTokenSilentCallback(token) {
-    // Catch chrome error if user is not authorized.
-    if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError.message);
-        showAuthNotification();
-        $('#authorize_button').show();
-    } else {
-        data = {
-            ...data,
-            token
-        };
-        console.log('Authentication success ', token);
-        $('#authorize_button').hide();
-    }
+	// Catch chrome error if user is not authorized.
+	if (chrome.runtime.lastError) {
+		console.error(chrome.runtime.lastError.message);
+		showAuthNotification();
+		$(CONSTANTS.IDS.AUTHORIZE_BTN).show();
+	} else {
+		data = {
+			...data,
+			token
+		};
+		console.log('Authentication success ', token);
+		$(CONSTANTS.IDS.AUTHORIZE_BTN).hide();
+	}
 }
 
 function showAuthNotification() {
-    var options = {
-        id: 'start-auth',
-        iconUrl: 'icon.png',
-        title: 'SNOW screenshot upload extension',
-        message: 'Click here to authorize access to GDrive'
-    };
-    createBasicNotification(options);
+	const options = {
+		id: 'start-auth',
+		iconUrl: 'icon.png',
+		title: 'SNOW screenshot upload extension',
+		message: 'Click here to authorize access to GDrive'
+	};
+	createBasicNotification(options);
 }
 
 function createBasicNotification(options) {
-    var notificationOptions = {
-        type: 'basic',
-        iconUrl: options.iconUrl, // Relative to Chrome dir or remote URL must be whitelisted in manifest.
-        title: options.title,
-        message: options.message,
-        isClickable: true
-    };
-    chrome.notifications.create(options.id, notificationOptions, function(notificationId) {});
+	const notificationOptions = {
+		type: 'basic',
+		iconUrl: options.iconUrl, // Relative to Chrome dir or remote URL must be whitelisted in manifest.
+		title: options.title,
+		message: options.message,
+		isClickable: true
+	};
+	chrome.notifications.create(options.id, notificationOptions, function (notificationId) { });
 }
 
 /**
@@ -219,44 +295,44 @@ function createBasicNotification(options) {
  * @param {string} token - Current users access_token.
  */
 function getAuthTokenInteractiveCallback(token) {
-    // Catch chrome error if user is not authorized.
-    if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        showAuthNotification();
-        $('#authorize_button').show();
-    } else {
-        data = {
-            ...data,
-            token
-        };
-        $('#authorize_button').hide();
-        console.log('Authentication success', token);
-    }
+	// Catch chrome error if user is not authorized.
+	if (chrome.runtime.lastError) {
+		console.error(chrome.runtime.lastError);
+		showAuthNotification();
+		$(CONSTANTS.IDS.AUTHORIZE_BTN).show();
+	} else {
+		data = {
+			...data,
+			token
+		};
+		$(CONSTANTS.IDS.AUTHORIZE_BTN).hide();
+		console.log('Authentication success', token);
+	}
 }
 
 /**
  *  Sign in the user upon button click.
  */
 function handleAuthClick(event) {
-    getAuthTokenInteractive();
+	getAuthTokenInteractive();
 }
 
 function getAuthTokenInteractive() {
-    getAuthToken({
-        interactive: true,
-        callback: getAuthTokenInteractiveCallback
-    });
+	getAuthToken({
+		interactive: true,
+		callback: getAuthTokenInteractiveCallback
+	});
 }
 
 /**
  * Triggered anytime user clicks on a desktop notification.
  */
 function notificationClicked(notificationId) {
-    // User clicked on notification to start auth flow.
-    if (notificationId === 'start-auth') {
-        getAuthTokenInteractive();
-    }
-    clearNotification(notificationId);
+	// User clicked on notification to start auth flow.
+	if (notificationId === 'start-auth') {
+		getAuthTokenInteractive();
+	}
+	clearNotification(notificationId);
 }
 
 /**
@@ -265,7 +341,7 @@ function notificationClicked(notificationId) {
  * @param {string} notificationId - Id of notification to clear.
  */
 function clearNotification(notificationId) {
-    chrome.notifications.clear(notificationId, function(wasCleared) {});
+	chrome.notifications.clear(notificationId, function (wasCleared) { });
 }
 
 /**
@@ -274,26 +350,57 @@ function clearNotification(notificationId) {
 chrome.notifications.onClicked.addListener(notificationClicked);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const btnGetData = document.getElementById('btn-get-data');
-    btnGetData.addEventListener('click', () => {
-        takeScreenshot();
-    });
+	const btnGetData = document.getElementById('take-screenshot-button');
+	btnGetData.addEventListener('click', () => {
+		takeScreenshot();
+	});
 
-    const authorizeButton = document.getElementById('authorize_button');
-    authorizeButton.addEventListener('click', () => {
-        handleAuthClick();
-    });
+	const authorizeButton = document.getElementById('authorize_button');
+	authorizeButton.addEventListener('click', () => {
+		handleAuthClick();
+	});
 
-    const callApiButton = document.getElementById('call_api_button');
-    callApiButton.addEventListener('click', () => {
-        getListOfSubFolders();
-    });
+	const callApiButton = document.getElementById('get_subfolders_button');
+	callApiButton.addEventListener('click', () => {
+		getListOfSubFolders();
+	});
 
-    const uploadButton = document.getElementById('upload_screenshot_btn');
-    uploadButton.addEventListener('click', () => {
-        uploadScreenshot();
-    });
+	const uploadButton = document.getElementById('upload_screenshot_button');
+	uploadButton.addEventListener('click', () => {
+		uploadScreenshot();
+	});
 
-    //Load Settings
-    loadSettings();
+	//Load Settings
+	loadSettings();
+});
+
+$.fn.extend({
+	disable: function () {
+		$(this).prop("disabled", true);
+		$(this).showSpinner();
+	},
+	enable: function () {
+		$(this).prop('disabled', false);
+		$(this).hideSpinner();
+	},
+	showSpinner: function () {
+		const position = $(this).position();
+		const elementWidth = $(this).outerWidth(true);
+		const elementHeight = $(this).outerHeight(true);
+
+		const top = position.top + elementHeight / 2 - 10;
+		const left = position.left + elementWidth;
+		$('#ajax-spinner')
+			.show()
+			.css({
+				top,
+				left
+			});
+
+		$(this).css({ paddingRight: 42 });
+	},
+	hideSpinner: function () {
+		$('#ajax-spinner').hide();
+		$(this).css({ paddingRight: 12 });
+	}
 });
